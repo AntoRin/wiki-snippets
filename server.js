0c9de40d430a20c269576084aab11b035c4b7e49
AntoRin <antorinish@gmail.com>
2021-04-19 22:16:04 +0530

    Fix bug where object wasn't destructured

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-const scraper = require("./scraper");
-const scraperFull = require("./scraper");
+const { scraper } = require("./scraper");
+const { scraperFull } = require("./scraper");
 
 const app = express();
 
